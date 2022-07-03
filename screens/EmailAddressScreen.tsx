c9c5116597aafebe024d9e0234465aab83ee9866
Alexander Epolite <alexander@epolite.net>
2022-07-03 14:06:18 -0700

    fix button alignment

--- screens/EmailAddressScreen.tsx
+++ screens/EmailAddressScreen.tsx
@@ -157,6 +157,7 @@ const styles = StyleSheet.create({
     buttonContainer: {
         alignItems: "center",
         display: "flex",
+        flexDirection: "row",
     },
     button: {
         borderStyle: "solid",
