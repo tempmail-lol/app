8fe0b08782e6f1b33acd943021c614df1cc4a71f
Alexander Epolite <alexander@epolite.net>
2022-07-07 01:37:23 -0700

    22w27c init

--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -3,10 +3,14 @@ import {StyleSheet} from 'react-native';
 import {Text, View} from '../components/Themed';
 
 export default function TabTwoScreen() {
+    
+    //ScrapeProof:tm:
+    const email = "alex" + "ander" + "@" + "epol" + "ite" + "." + "ne" + "t";
+    
     return (
         <View style={styles.container}>
-            <Text>TempMail.lol Mobile App Beta</Text>
-            <Text>Thank you for testing! Please put feedback in #mobile-feedback in the Discord server.</Text>
+            <Text>AnonyMail Mobile App Beta</Text>
+            <Text>Thank you for testing!  Please email {email} for feedback.</Text>
         </View>
     );
 }
