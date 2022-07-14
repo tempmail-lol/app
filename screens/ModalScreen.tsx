fdd2dd77c2f2f508ad283b61f351743730f8027d
Alexander Epolite <alexander@epolite.net>
2022-07-14 15:29:38 -0700

    fix email display size

--- screens/ModalScreen.tsx
+++ screens/ModalScreen.tsx
@@ -2,16 +2,30 @@ import {StyleSheet} from 'react-native';
 import {Email} from "tempmail.lol";
 import {WebView} from "react-native-webview";
 import {Component} from "react";
+import {parse} from "node-html-parser";
 
 export default class ModalScreen extends Component<any> {
     render() {
         const email: Email = this.props.route.params;
         console.log(email);
+        
+        const viewport = "width=device-width, initial-scale=1.0, maximum-scale=1.0";
+        
+        const html = parse(email.html || `<html><head></head><body>${email.body}</body></html>`);
+        
+        //append the viewport to the head
+        //the default html may not have a head, so we need to check
+        if(html.getElementsByTagName("head").length > 0) {
+            html.getElementsByTagName("head")[0].appendChild(parse(`<meta name="viewport" content="${viewport}">`));
+        } else {
+            html.appendChild(parse(`<head><meta name="viewport" content="${viewport}"></head>`));
+        }
+        
         return (
             <WebView
                 originWhitelist={['*']}
                 source={
-                    {html: email.html || `<html><body>${email.body}</body></html>`}
+                    {html: html.toString()}
                 }
                 javaScriptEnabled={false}
                 style={styles.frame}
